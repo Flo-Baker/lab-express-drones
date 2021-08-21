e0d6ba9f6f91b981d10ec10979cca0c553249fa8
Florian Becker <becker_florian@online.de>
2021-08-21 17:07:27 +0200

    solved iteration 1, 2

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,16 @@
 // Iteration #1
+// 1.
+const { Schema, model } = require('mongoose');
+
+const dronesSchema = new Schema(
+  {
+    name: { type: String },
+    propellers: { type: Number },
+    maxSpeed: { type: Number },
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model("Drone", dronesSchema);
--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((drones) => {
+      console.log("Here is the list of drones", drones);
+      res.render("drones/list.hbs", { drones });
+    })
+    .catch((err) => {
+      console.log("An error occured while listing all the drones.");
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
+// get is only to show the form
+router.get("/drones/create-form", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form.hbs");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create-form", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  Drone.create()
+    .then((drones) => {
+      console.log("Here is the new drone you have created.", drones);
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log("An error has occured while creating a new drone.");
+    });
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
